7991a70150fa5f724ef015025b53de5f3d343c6f
Stefan Gräwe <me@sgraewe.de>
2018-05-27 11:32:19 +0200

    Add PurgeCSS support

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,3 +1,7 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
 module.exports = {
   /*
   ** Headers of the page
@@ -36,6 +40,10 @@ module.exports = {
   */
   build: {
     /*
++    ** Extract CSS
++    */
+    extractCSS: true,
+    /*
     ** Run ESLint on save
     */
     extend (config, { isDev, isClient }) {
@@ -46,6 +54,22 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
+      }
+
+      /*
+      ** Cleanup CSS with PurgeCSS
+      */
+      if (!isDev) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body']
+          })
+        )
       }
     }
   }
