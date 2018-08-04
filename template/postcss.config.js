b28652ba37a9c7a89412e7d692577800b565f2f5
Stefan Gräwe <me@sgraewe.de>
2018-08-04 13:05:37 +0200

    Update autoprefixer for better css-grid support

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -3,6 +3,10 @@ module.exports = {
     require('postcss-import'),
     require('postcss-url'),
     require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer')({
+      cascade: false,
+      grid: true
+    }),
     require('postcss-preset-env')({
       stage: 0
     }),
